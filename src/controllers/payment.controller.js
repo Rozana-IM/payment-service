2cd2388a96f997dd9d99b5ebadae0727c72603c1
Rozana-IM <rozanaim09@gmail.com>
2026-03-10 20:52:06 +0530

    Update payment.controller.js

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -43,3 +43,46 @@ res.status(500).json({error:"Payment creation failed"});
 }
 
 };
+exports.verifyPayment = async (req,res)=>{
+
+const { orderId, razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+try{
+
+const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+const expectedSignature = crypto
+.createHmac("sha256", process.env.RAZORPAY_SECRET)
+.update(body.toString())
+.digest("hex");
+
+if(expectedSignature === razorpay_signature){
+
+// ✅ PAYMENT VALID
+
+await axios.post(`${process.env.ORDER_SERVICE_URL}/orders/payment-update`,{
+orderId,
+status:"PAID"
+});
+
+return res.json({
+message:"Payment verified successfully"
+});
+
+}
+
+return res.status(400).json({
+error:"Payment verification failed"
+});
+
+}catch(err){
+
+console.error("Verification error:",err.message);
+
+res.status(500).json({
+error:"Payment verification failed"
+});
+
+}
+
+};
