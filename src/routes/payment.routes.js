a605e26fd37d1a286c7dbdb34991f3eb8fe05676
Rozana-IM <rozanaim09@gmail.com>
2026-03-10 20:50:32 +0530

    Update payment.routes.js

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const { verifyToken } = require("../middleware/auth.middleware");
 
-const { createPayment } = require("../controllers/payment.controller");
+const {
+  createPayment,
+  verifyPayment
+} = require("../controllers/payment.controller");
 
 router.post("/payments/create", verifyToken, createPayment);
+
+router.post("/payments/verify", verifyToken, verifyPayment);
 
 module.exports = router;
