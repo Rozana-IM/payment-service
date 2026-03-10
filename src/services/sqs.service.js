dab3053128f5ad5928fec122291e8890a0b72735
Rozana-IM <rozanaim09@gmail.com>
2026-03-10 21:59:48 +0530

    Create sqs.service.js

---
+++ src/services/sqs.service.js
@@ -0,0 +1,17 @@
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+
+const sqs = new SQSClient({
+  region: process.env.AWS_REGION
+});
+
+exports.sendPaymentEvent = async (event) => {
+
+  const params = {
+    QueueUrl: process.env.SQS_QUEUE_URL,
+    MessageBody: JSON.stringify(event)
+  };
+
+  const command = new SendMessageCommand(params);
+
+  return await sqs.send(command);
+};
