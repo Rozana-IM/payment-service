4f295df5104e2ca9d20904ae1f05b71c28839e1e
Rozana-IM <rozanaim09@gmail.com>
2026-03-12 01:51:29 +0530

    Update razorpay.service.js

--- src/services/razorpay.service.js
+++ src/services/razorpay.service.js
@@ -5,12 +5,16 @@ const razorpay = new Razorpay({
   key_secret: process.env.RAZORPAY_SECRET
 });
 
-exports.createRazorpayOrder = async (amount) => {
+exports.createRazorpayOrder = async (orderId, amount) => {
 
   const options = {
     amount: amount * 100,
     currency: "INR",
-    receipt: "lucci_order_" + Date.now()
+    receipt: `lucci_order_${orderId}`,
+
+    notes: {
+      orderId: orderId
+    }
   };
 
   return await razorpay.orders.create(options);
