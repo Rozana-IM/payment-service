1ad453364bf5495590706a0044255c120d061099
Rozana-IM <rozanaim09@gmail.com>
2026-03-12 01:52:03 +0530

    Update sqs.service.js

--- src/services/sqs.service.js
+++ src/services/sqs.service.js
@@ -8,7 +8,10 @@ exports.sendPaymentEvent = async (event) => {
 
   const params = {
     QueueUrl: process.env.SQS_QUEUE_URL,
-    MessageBody: JSON.stringify(event)
+    MessageBody: JSON.stringify(event),
+
+    MessageGroupId: "payments",
+    MessageDeduplicationId: event.orderId + "-" + Date.now()
   };
 
   const command = new SendMessageCommand(params);
