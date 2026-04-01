7f52be5ca9ccc1bd0c82e8ecedcc4b164da48cef
Rozana-IM <rozanaim09@gmail.com>
2026-04-01 21:29:02 +0530

    Update razorpay.service.js

--- src/services/razorpay.service.js
+++ src/services/razorpay.service.js
@@ -1,19 +1,20 @@
 const Razorpay = require("razorpay");
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY,
-  key_secret: process.env.RAZORPAY_SECRET
-});
-
 exports.createRazorpayOrder = async (orderId, amount) => {
+
+  // 🔥 CREATE INSTANCE INSIDE FUNCTION (IMPORTANT)
+  const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET
+  });
+
+  console.log("RAZORPAY KEY:", process.env.RAZORPAY_KEY); // debug
 
   const options = {
     amount: amount * 100,
     currency: "INR",
     receipt: `lucci_order_${orderId}`,
-    notes: {
-      orderId
-    }
+    notes: { orderId }
   };
 
   return razorpay.orders.create(options);
